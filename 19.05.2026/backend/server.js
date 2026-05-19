1bc055b3646e39885dbc62de2e15f036bc1536c5
luiza <luiza_s_rocha@estudante.sesisenai.org.br>
2026-05-19 15:47:33 -0300

    commit_2

---
+++ 19.05.2026/backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import 'dotenv/config'
+
+
+const app = express()
+const port = process.env.API_PORT
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Servidor ON em http://localhost:${port}`)
+})
