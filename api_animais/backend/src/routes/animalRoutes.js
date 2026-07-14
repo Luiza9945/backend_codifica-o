102a577446948ffa0e2f860db7ba60c5b49d1ab4
luiza <luiza_s_rocha@estudante.sesisenai.org.br>
2026-07-14 14:45:27 -0300

    a

--- api_animais/backend/src/routes/animalRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import {animalController} from "../controllers/animalController.js"
-
-const router = Router();
-
-router.get('/animais', animalController.getAll);
-router.post('/animais')
